022bb329fcf8d0586f9d7d738e42deabcdfab17c
elajasho <elaja.sho@gmail.com>
2023-09-15 18:23:17 +0300

    updated search body for final check

--- src/test/java/SearchSelenideTest.java
+++ src/test/java/SearchSelenideTest.java
@@ -28,7 +28,17 @@ public class SearchSelenideTest {
 
         open("/selenide/selenide/wiki/SoftAssertions");
 
-        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
 
     }
 }
