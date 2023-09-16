711e72dcbc18373aa9fcee07bae933393ae8cbd1
elajasho <elaja.sho@gmail.com>
2023-09-16 19:46:29 +0300

    added HoverGitTest.java. lesson 5 homework pt.1

---
+++ src/test/java/HoverGitTest.java
@@ -0,0 +1,19 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HoverGitTest {
+
+    @Test
+    void hoverTest() {
+        open("https://github.com/");
+        $(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+
+        $(".font-mktg").shouldHave(Condition.text("Build like the best"));
+
+    }
+
+}
