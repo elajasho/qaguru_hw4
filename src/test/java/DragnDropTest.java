7ac56746ee3d53686386a9fdcd8247e156453b7f
elajasho <elaja.sho@gmail.com>
2023-09-16 20:34:15 +0300

    added DragnDropTest.java. lesson 5 homework pt.2

---
+++ src/test/java/DragnDropTest.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragnDropTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1300x900";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void ddTestv1() {
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").$("header").shouldHave(text("A"));
+        $("#column-a").dragAndDrop(to("#column-b"));
+        $("#column-a").$("header").shouldHave(text("B"));
+    }
+}
